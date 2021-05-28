863ad318893a14d0e782b2cd714d6c09b5e3a5e6
utkukalkanli319 <>
2021-05-28 20:15:42 +0300

    webview eklendi ve node-red server'ına bağlanıyor

--- app/src/main/java/com/example/airify/DataActivity.java
+++ app/src/main/java/com/example/airify/DataActivity.java
@@ -3,12 +3,33 @@ package com.example.airify;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class DataActivity extends AppCompatActivity {
-
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowContentAccess(true);
+        webSettings.setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://node-red-oqurx-2021-05-27.eu-gb.mybluemix.net/ui/#!/0?socketid=6kiVHsPwOToAMqAgAAAJ");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
     }
 }
--- app/src/main/java/com/example/airify/MainActivity.java
+++ app/src/main/java/com/example/airify/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.airify;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final int INTERNET_PERMISSON = 12;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, 2);
     }
 
     public void onClickObserve(View view) {
@@ -22,5 +32,19 @@ public class MainActivity extends AppCompatActivity {
     public void onClickInfo(View view) {
         Intent infoActivity = new Intent(this, InfoActivity.class);
         startActivity(infoActivity);
+    }
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 2: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //Toast.makeText(getApplicationContext(), "Access to Internet Permission Granted. Thanks.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Access to Internet Permission Denied.", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
     }
 }
